8c2f057cb90361497972d9fc2e81e692d5c02c9e
surendra44 <71259552+surendra44@users.noreply.github.com>
2023-01-22 23:35:40 +0530

    socket in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import  {io} from 'socket.io-client';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatApp';
+  messages =[]
+  newusername = prompt('enter user name')
+  private socket;
+  constructor() {
+      this.socket = io('http://localhost:3000');
+  }
+  newUser(){
+  this.socket.emit('new-user-joined', this.newusername)
+    console.log(this.newusername);
+  
+  }
+  listenmsg(){
+  this.socket.on('receive', (data) => {
+    console.log(data);
+  });
+  }
+  sendmsg(data:any){
+    console.log(data,'input msg')
+    this.socket.emit('send', data);
+  }
+
 }
