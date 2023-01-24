a50b3d3700d215ef0950e97e37ae21e3a0df5ddf
surendra44 <71259552+surendra44@users.noreply.github.com>
2023-01-24 17:01:36 +0530

    version2.0 updts

--- src/app/socketio.service.ts
+++ src/app/socketio.service.ts
@@ -1,23 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import  {io} from 'socket.io-client';
 @Injectable({
   providedIn: 'root'
 })
 export class SocketioService {
 messages:any = []
 username:any;
-public socket;
-constructor() {
-    this.socket = io('http://localhost:8000');
-  }
-  newUser(newusername:any){
-    this.socket.emit('new-user-joined',newusername)
-    console.log(newusername);
-  }
-  
-  sendmsg(data:any){ 
-    console.log(data.value,'input msg')
-    this.socket.emit('send', data.value);
-  }
+
 }
